f250a23e54f09094739e5d4341f49cd025d1ea52
Daniil <daniilkharitonov1990@gmail.com>
2022-11-03 23:37:15 +0500

    Was added Example 2 checking

---
+++ Example_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два числа");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberB * numberB == numberA)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("Folse");
+}
